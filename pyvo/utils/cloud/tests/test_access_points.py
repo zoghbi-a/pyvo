2369af5a5d426e9cb8cfe0089bc9f3f50d3ff14a
Abdu Zoghbi <a.zoghbi@nasa.gov>
2023-04-10 09:13:34 -0400

    changed id to uid in access_points, fixed datalinks

--- pyvo/utils/cloud/tests/test_access_points.py
+++ pyvo/utils/cloud/tests/test_access_points.py
@@ -32,8 +32,8 @@ class TestPREMAccessPoint(unittest.TestCase):
         cls.url = 'https://www.ivoa.net/xml/ADQL/ADQL-v1.0.xsd'
         cls.ap = PREMAccessPoint(cls.url)
         
-    def test_id(self):
-        self.assertEqual(self.ap.id, self.url)
+    def test_uid(self):
+        self.assertEqual(self.ap.uid, self.url)
         
     def test_provider(self):
         self.assertEqual(PREMAccessPoint.provider, 'prem')
@@ -55,30 +55,30 @@ class TestAWSAccessPoint(unittest.TestCase):
     def test_provider(self):
         self.assertEqual(AWSAccessPoint.provider, 'aws')
     
-    def test_uri_is_None(self):
-        """uri is None"""
-        # uri is None, and both bucket_name and key are None
+    def test_uid_is_None(self):
+        """uid is None"""
+        # uid is None, and both bucket_name and key are None
         with self.assertRaises(ValueError):
-            ap = AWSAccessPoint(uri=None, bucket_name=None, key=None)
+            ap = AWSAccessPoint(uid=None, bucket_name=None, key=None)
         
-        # uri is None, and either bucket_name and key are None
+        # uid is None, and either bucket_name and key are None
         with self.assertRaises(ValueError):
-            ap = AWSAccessPoint(uri=None, bucket_name='somebucket', key=None)
+            ap = AWSAccessPoint(uid=None, bucket_name='somebucket', key=None)
             
         with self.assertRaises(ValueError):
-            ap = AWSAccessPoint(uri=None, bucket_name=None, key='some/key')
+            ap = AWSAccessPoint(uid=None, bucket_name=None, key='some/key')
         
-    def test_uri(self):
-        """uri is passed"""
+    def test_uid(self):
+        """uid is passed"""
         
         with self.assertRaises(ValueError):
-            ap = AWSAccessPoint(uri='s5://somebucket/file')
+            ap = AWSAccessPoint(uid='s5://somebucket/file')
             
         ap = AWSAccessPoint(bucket_name='bucket', key='key/file')
         self.assertEqual(ap.s3_uri, 's3://bucket/key/file')
         
     def test_bucket_name(self):
-        ap = AWSAccessPoint(uri='s3://bucket/key/file')            
+        ap = AWSAccessPoint(uid='s3://bucket/key/file')            
         self.assertEqual(ap.s3_bucket_name, 'bucket')
         
         ap = AWSAccessPoint(bucket_name='bucket2', key='key/file')
@@ -99,20 +99,20 @@ class TestAccessPointContainer(unittest.TestCase):
     
     def test_2_arg(self):
         ap1 = AccessPoint()
-        ap2 = PREMAccessPoint(url='http://some/url')
+        ap2 = PREMAccessPoint(uid='http://some/url')
         apc = AccessPointContainer(ap1, ap2)
         self.assertEqual(len(apc.access_points), 2)
         
     def test_2_arg_same_ap(self):
-        ap1 = PREMAccessPoint(url='http://some/url')
-        ap2 = PREMAccessPoint(url='http://some/url2')
+        ap1 = PREMAccessPoint(uid='http://some/url')
+        ap2 = PREMAccessPoint(uid='http://some/url2')
         apc = AccessPointContainer(ap1, ap2)
         self.assertEqual(len(apc.access_points), 1)
         self.assertEqual(len(list(apc.access_points.values())[0]), 2)
         
-    def test_2_arg_repeat_id(self):
-        ap1 = PREMAccessPoint(url='http://some/url')
-        ap2 = PREMAccessPoint(url='http://some/url')
+    def test_2_arg_repeat_uid(self):
+        ap1 = PREMAccessPoint(uid='http://some/url')
+        ap2 = PREMAccessPoint(uid='http://some/url')
         apc = AccessPointContainer(ap1, ap2)
         self.assertEqual(len(apc.access_points), 1)
         self.assertEqual(len(list(apc.access_points.values())[0]), 1)
